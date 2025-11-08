5c7502d1f4a6a35fabf9af11fe0326c2f6c781b9
Rohan Tomar <159640350+Rohan-218@users.noreply.github.com>
2025-11-08 22:39:41 +0530

    minor fix

--- server/src/models/schemas/security/signupSchema.js
+++ server/src/models/schemas/security/signupSchema.js
@@ -7,8 +7,7 @@ export default Joi.object(((messageKey) => ({
   name: requiredStringValidator(messageKey, 'name'),
   email: requiredEmailValidator(messageKey, 'email'),
   password: requiredStringValidator(messageKey, 'password'),
-  phoneNumber: requiredPhoneNumberValidator(messageKey, 'phoneNumber'),
-  role: Joi.string().required()
+  phoneNumber: requiredPhoneNumberValidator(messageKey, 'phoneNumber')
     .messages({
       'any.required': `${messageKey}.role is required`,
       'number.base': `${messageKey}.role must be a string`,
